c05f0042ecad2427d60296f2951ea2286f488998
izg0820 <33209133+izg0820@users.noreply.github.com>
2018-05-10 16:42:26 +0900

    Modify UI design

---
+++ EumMe/app/src/main/java/com/example/android/eumme/audioControl/Constants.java
@@ -0,0 +1,37 @@
+package com.example.android.eumme.audioControl;
+
+import android.media.AudioFormat;
+import android.media.MediaRecorder;
+
+public class Constants {
+    private static int audioSource = MediaRecorder.AudioSource.MIC;
+    private static int sampleRateInHz = 8000;
+    private static int channelConfig = AudioFormat.CHANNEL_IN_MONO;
+    private static int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+    private static int BufferElements2Rec = 1024;
+    private static int BytesPerElement = 2;
+
+    public static int getAudioSource() {
+        return audioSource;
+    }
+
+    public static int getSampleRateInHz(){
+        return sampleRateInHz;
+    }
+
+    public static int getChannelConfig(){
+        return channelConfig;
+    }
+
+    public static int getAudioFormat() {
+        return audioFormat;
+    }
+
+    public static int getBufferElements2Rec() {
+        return BufferElements2Rec;
+    }
+
+    public static int getBytesPerElement() {
+        return BytesPerElement;
+    }
+}
