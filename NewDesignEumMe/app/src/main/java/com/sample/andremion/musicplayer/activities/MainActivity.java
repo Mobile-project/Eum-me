1cf9688813ddbb723ab2663adeae1091dc156356
didi0410 <taristmas@gmail.com>
2018-05-27 23:38:05 +0900

    add getcreatedtime and getplaytime

--- NewDesignEumMe/app/src/main/java/com/sample/andremion/musicplayer/activities/MainActivity.java
+++ NewDesignEumMe/app/src/main/java/com/sample/andremion/musicplayer/activities/MainActivity.java
@@ -30,18 +30,17 @@ import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Chronometer;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.sample.andremion.musicplayer.DB.DBHelper;
-import com.sample.andremion.musicplayer.memoControl.MemoFragement;
 import com.sample.andremion.musicplayer.R;
 import com.sample.andremion.musicplayer.RecordingMataData;
 import com.sample.andremion.musicplayer.audioControl.Constants;
 import com.sample.andremion.musicplayer.audioControl.RecordeService;
+import com.sample.andremion.musicplayer.memoControl.MemoFragement;
 import com.sample.andremion.musicplayer.memoItem;
 
 import java.io.File;
@@ -265,6 +264,11 @@ public class MainActivity extends AppCompatActivity {
         public int getCount() {
             return Integer.MAX_VALUE;
         }
+    }
+
+    public void setDateAndCreatedTime(String date, String time){
+
+
     }
 }
 
