abcd1810cd8e7b416dec9e149342ff64aef27f3e
izg0820 <33209133+izg0820@users.noreply.github.com>
2018-05-31 23:00:02 +0900

    Modify viewpager adapter

--- NewDesignEumMe/app/src/main/java/com/sample/andremion/musicplayer/Presenter/ViewPagerAdapter.java
+++ NewDesignEumMe/app/src/main/java/com/sample/andremion/musicplayer/Presenter/ViewPagerAdapter.java
@@ -15,17 +15,12 @@ public class ViewPagerAdapter extends FragmentStatePagerAdapter {
     int count=1;
     @Override
     public Fragment getItem(int position) {
-        if(position>0||position<Integer.MAX_VALUE){
-            Fragment fragment = new MemoFragement();
-            return fragment;
 
-        }
-        else
-            return null;
+            Fragment fragment = new MemoFragement().create(position);
+            return fragment;
     }
     @Override
     public int getCount() {
-        Log.d(tag, "Getcount");
         return Integer.MAX_VALUE;
     }
 }
